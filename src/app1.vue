36851a25152bff5ff0791b7ae109b7c8e80fc4d1
wangsheng122611 <ws498436829@qq.com>
2017-07-04 11:26:56 +0800

    项目结构初

---
+++ src/app1.vue
@@ -0,0 +1,28 @@
+<template>
+
+
+
+</template>
+<script>
+export default{
+    data(){
+        return{
+            msg:  "hell word"
+        }
+    },
+    methods:{
+
+
+    },
+}
+
+
+
+</script>
+
+
+<style>
+
+
+
+</style>
